6175dea7754ae8aeba5da0006276ccac610b1de4
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-07-04 17:13:01 +0300

    feature: add canvas and animate character

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './index.scss';
+
+import SenseiWalk from './assets/Female-2-Walk.png';
+
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d');
+const spriteW = 48;
+const spriteH = 48;
+const shots = 3;
+let cycle = 0;
+
+const img = document.createElement('img');
+img.src = SenseiWalk;
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    cycle = (cycle + 1) % shots;
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100);
+  }, 120);
+});
