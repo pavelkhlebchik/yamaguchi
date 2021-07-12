c403f6c8b2138ff276057ca7603165bb3fd8d997
Pavel <pavelkhlebchik@gmail.com>
2021-07-12 11:59:22 +0300

    feature: add init game and game core

---
+++ src/client/ClientGame.js
@@ -0,0 +1,28 @@
+import ClientEngine from './ClientEngine';
+
+class ClientGame {
+  constructor(cfg) {
+    Object.assign(this, {
+      cfg,
+    });
+
+    this.engine = this.ClientEngine();
+  }
+
+  ClientEngine() {
+    return new ClientEngine(document.getElementById(this.cfg.tagId));
+  }
+
+  initEngine() {
+    this.engine.start();
+  }
+
+  static init(cfg) {
+    if (!ClientGame.game) {
+      ClientGame.game = new ClientGame(cfg);
+    }
+    console.log('Game INIT');
+  }
+}
+
+export default ClientGame;
