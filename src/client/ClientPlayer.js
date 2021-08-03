b5cd61d337e23848cc996e8612e003b103a7a66e
Pavel <pavelkhlebchik@gmail.com>
2021-08-03 11:48:42 +0300

    feature: add random playerName from login form

--- src/client/ClientPlayer.js
+++ src/client/ClientPlayer.js
@@ -4,7 +4,7 @@ class ClientPlayer extends ClientGameObject {
   constructor(cfg) {
     super(cfg);
 
-    this.playerName = 'Zar';
+    this.playerName = document.getElementById('name').value;
 
     const { world } = cfg.cell;
 
